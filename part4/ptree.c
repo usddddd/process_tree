096847b4dbe2b7c67b15ad7337d1a1fccb24bf57
Adam Fowler <ajf2177@columbia.edu>
2014-02-12 05:41:25 -0500

    Removed stack based DFS and used recursion

--- part4/ptree.c
+++ part4/ptree.c
@@ -3,64 +3,53 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
 
-    // stack handle
-    static LIST_HEAD(process_stack);
+    // char array to hold tab offset of child processes [limited to 20 levels with 1 for null terminator]
+    char tab_offset[21];
+    // char array to hold symbol for children
+    char child[] = "\\_";
+
+
     
-    static void print_task_info(struct task_struct *t){
+    static void print_task_info(struct task_struct *t, int level){
         struct task_struct *task;
         struct list_head *list;
+        char *buffer = kmalloc(sizeof(char)*100);
 
-        printk(KERN_INFO "Name: %s  State: %ld  PID: %ld \n", t->comm,t->state,(long)t->pid);
+        // construct offset for tree display
+        int i;
+        for(i = 0; i < level && i < 20; i++){
+            tab_offset[i] = '\t';
+        }
+        // set null terminator
+        tab_offset[i] = '\0';
 
+        // write task info to buffer
+        if(level > 0){
+        snprintf(buffer, sizeof(buffer), "%s \\_Name: %s  State: %ld  PID: %ld \n",tab_offset, t->comm,t->state,(long)t->pid);
+        }
+        else{
+        snprintf(buffer, sizeof(buffer), "--Name: %s  State: %ld  PID: %ld \n",tab_offset, t->comm,t->state,(long)t->pid);
+        }
+        // print tree to kernel log
+        printk(KERNINFO "%s", buffer);
         // iterate through children of init process
         // init_task is list_head not a &list_head
         // book is wrong! use '.' not '->'
         list_for_each(list, &t->children){
             task = list_entry(list, struct task_struct, sibling);
-            print_task_info(task);
+            print_task_info(task, level+1);
         }
     }
 
-    // push onto stack represented by process_stack
-    //void push_to_stack(struct list_head *new){
-      //  list_add(new, &process_stack);
-   // }
-
-    // pop from process_stack
-    //struct task_struct *pop_stack(void){
-      //  struct task_struct *task;
-        // get first elem from stack
-        //struct list_head *l = process_stack.next;
-        //task = list_entry(l, struct task_struct, sibling);
-        // remove entry from stack
-        //list_del(l);
-        //return task_struct popped from stack
-        //return task;
-   // }
 
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
 
-    // char array to hold tab offset of child processes [limited to 20 levels with 1 for null terminator]
-    char tab_offset[21] = "\t";
-    // char array to hold symbol for children
-    char child[] = "\\_";
 
     // print init info as first entry in process tree
     printk(KERN_INFO "-- ");
-    print_task_info(&init_task);
-
-
-    // DFS through children of init and their children
-   // while(!list_empty(&process_stack)){
-     //   task = pop_stack();
-       // print_task_info(task);
-        // add children of task to stack
-        //list_for_each(list, &task->children){
-          //  push_to_stack(list);
-        //}
-     //}
+    print_task_info(&init_task, 0);
 
     printk(KERN_INFO "Done Loading Module\n");
 
